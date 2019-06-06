a76e05d47233801837bfb85d1ca6a727d441e822
anotida <amaditsha@gmail.com>
2019-06-06 16:10:53 +0200

    Initial commit

--- app/src/main/java/zw/co/vokers/tracker_location/MainActivity.java
+++ app/src/main/java/zw/co/vokers/tracker_location/MainActivity.java
@@ -1,7 +1,9 @@
 package zw.co.vokers.tracker_location;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         final Button startTrackingBtn = findViewById(R.id.startTrackingBtn);
         final Button viewLocationBtn = findViewById(R.id.viewLocationBtn);
@@ -32,10 +36,15 @@ public class MainActivity extends AppCompatActivity {
         viewLocationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("View", "View tracking location ...");
-                Toast toast = Toast.makeText(v.getContext(), "+9023902340290, -430430940940349000", Toast.LENGTH_LONG);
-                toast.show();
-                viewlocationText.setText("+9023902340290, -430430940940349000");
+                Log.i("View", "Go to map activity ...");
+
+
+
+                Intent mapIntent = new Intent(v.getContext(), MapsActivity.class);
+                mapIntent.putExtra("latitude", "-17.807184");
+                mapIntent.putExtra("longitude", "31.129556");
+                startActivity(mapIntent);
+//                viewlocationText.setText("+9023902340290, -430430940940349000");
 
             }
         });
